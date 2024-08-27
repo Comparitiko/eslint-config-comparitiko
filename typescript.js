8933eb61b80cc135a1c199ddeec6c1d0e7f1c929
Comparitiko <gabigcy@gmail.com>
2024-08-27 13:14:27 +0200

    Added some rules for typescript

--- typescript.js
+++ typescript.js
@@ -9,6 +9,12 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   { files: ["**/*.{ts}"] },
   {
-    rules: {},
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": "error",
+      "@typescript-eslint/prefer-string-starts-ends-with": "error",
+      "@typescript-eslint/no-duplicate-enum-values": "error",
+    },
   },
 ];
