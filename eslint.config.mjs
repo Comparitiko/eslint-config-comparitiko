e703001ea81a6e4ceb2d5a23a32b0eb415beda2f
Comparitiko <gabigcy@gmail.com>
2024-08-27 13:14:13 +0200

    Added some rules and unused imports plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginImport from "eslint-plugin-import";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
 
 export default [
@@ -15,9 +16,10 @@ export default [
     plugins: {
       "simple-import-sort": simpleImportSort,
       import: eslintPluginImport,
+      "unused-imports": unusedImports,
     },
     rules: {
-      "no-unused-vars": "error",
+      "no-unused-vars": "off",
       "no-undef": "error",
       "no-console": "warn",
       "prefer-const": "error",
@@ -25,14 +27,32 @@ export default [
       "capitalized-comments": ["error", "always"],
       curly: ["error", "multi-or-nest"],
       eqeqeq: ["error", "always"],
+      "prefer-template": "error",
       "no-unneeded-ternary": "error",
       "no-unreachable-loop": "error",
       "no-use-before-define": "error",
       "no-useless-assignment": "error",
+      "no-useless-rename": "error",
+      "no-useless-return": "error",
+      "no-throw-literal": "error",
+      // Plugins configuration
+      // Simple-import-sort
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      // Eslint-plugin-import
       "import/first": "error",
       "import/no-duplicates": "error",
+      // Unused-imports
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ];
